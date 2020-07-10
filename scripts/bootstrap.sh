e0d4fb128fd02dfad541d56ceec1bcb89ac0b800
goeran <mail@goeran.no>
2020-07-10 19:58:29 +0000

    Added scripts

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Bootstrap my dotfiles
+
+# Exit immediately if a command exits with non-error status
+set -e
+
+cd "$(dirname $0)"/..
+
+# Run installers
+echo "About to run installers"
+./scripts/install
+echo "Completed running installers"
+
+# Run setups
+echo "About to run setup scripts"
+./git/setup.sh
+./vim/setup.sh
+echo "Completed running setup scripts"
+
