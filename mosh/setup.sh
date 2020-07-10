2f5bd08e79c9d53e04c488e40756a63708b11d7f
goeran <mail@goeran.no>
2020-07-10 08:13:57 +0000

    Made mosh install script idempotent

--- mosh/setup.sh
+++ mosh/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-sudo add-apt-repository ppa:keithw/mosh
-sudo apt-get update
-sudo apt-get install mosh
+if ! [ -x "$(command -v mosh)" ]; then
+    echo "Install mosh"
+    sudo add-apt-repository ppa:keithw/mosh
+    sudo apt-get update
+    sudo apt-get install mosh
+else
+    echo "Mosh already installed"
+fi
 
